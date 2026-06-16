42a98b8fec972a74f28f9b23ac0aa131a2482f43
VietDucK53VB2 <ductrinhviet05@gmail.com>
2026-06-16 23:26:39 +0700

    keep reader login on N3 domain

--- backend/wwwroot/ui/reader/assets/VSpacer-BnjoHDK0.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./VAvatar-DW1hNSFC.js";const r=a("v-spacer","div","VSpacer");export{r as V};
---
+++ backend/wwwroot/ui/reader/assets/VSpacer-CMhDrMiT.js
@@ -0,0 +1,1 @@
+import{B as a}from"./VAvatar-BjQMg4Gv.js";const r=a("v-spacer","div","VSpacer");export{r as V};
